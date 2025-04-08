5265d59ce9b4a21984b14eb1a695cc751d7b5645
Pablo Zapata <tamtok2.0@gmail.com>
2025-04-07 19:13:24 -0600

    String de conexion a la bd en Aiven y gitignore

--- ApiReto/Controllers/VideojuegoController.cs
+++ ApiReto/Controllers/VideojuegoController.cs
@@ -6,5 +6,8 @@ namespace ApiReto.Controllers;
 [Route("[controller]")]
 public class VideojuegoController : ControllerBase
 {
+    string connectionString = "Server=mysql-93cf659-tamtok2-09a8.b.aivencloud.com;Port=23481;Database=BDOxxo;Uid=avnadmin;Password=AVNS_-9SXvTjsy8x6dg2kaJR";
+
     
+
 }
