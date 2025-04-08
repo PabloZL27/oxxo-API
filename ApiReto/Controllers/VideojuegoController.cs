eb3ed65e25dbbf05753173b969b557f043fb1c72
José Pablo Zapata Lozano <165724276+PabloZL27@users.noreply.github.com>
2025-04-07 18:53:33 -0600

    Add files via upload

---
+++ ApiReto/Controllers/VideojuegoController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiReto.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VideojuegoController : ControllerBase
+{
+    
+}
---
+++ ApiReto/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Forzar redirección a HTTPS
+builder.Services.AddHttpsRedirection(options =>
+{
+options.HttpsPort = 7058; // Puerto definido en launchSettings https
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
