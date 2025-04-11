c130a80222813753762f3f70b8d7ceada40ded4d
Jordy Granados <cjgranadosc@gmail.com>
2025-04-11 16:30:12 -0600

    BLAH BLAH BLAH

--- ApiReto/obj/Debug/net8.0/ApiReto.AssemblyInfo.cs
+++ ApiReto/obj/Debug/net8.0/ApiReto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiReto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7385a32a7c43b50499409706e0c8862a0806aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370c9f049d9f6de0dedfc47d3c0b92216f8ec435")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiReto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiReto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
